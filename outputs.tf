b34bdff0299bc21dd8eb786e5bf2644b665b9a9c
Gabriel Chamon Araujo <gchamon@live.com>
2019-09-28 22:50:27 -0300

    formatted whole code using fmt -recursive

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,34 @@
 output "definitions-bucket" {
   description = "The bucket created to store de antivirus definitions"
-  value = aws_s3_bucket.antivirus-definitions
+  value       = aws_s3_bucket.antivirus-definitions
 }
 
 output "scanner-function" {
   description = "The created scanner function resource"
-  value = aws_lambda_function.antivirus-scanner
+  value       = aws_lambda_function.antivirus-scanner
 }
 
 output "update-function" {
   description = "The created definitions update function resource"
-  value = aws_lambda_function.antivirus-update
+  value       = aws_lambda_function.antivirus-update
 }
 
 output "scanner-function-role" {
   description = "The role used by the scanner function"
-  value = aws_iam_role.antivirus-scanner-role
+  value       = aws_iam_role.antivirus-scanner-role
 }
 
 output "update-function-role" {
   description = "The role used by the definitions update function"
-  value = aws_iam_role.antivirus-update-role
+  value       = aws_iam_role.antivirus-update-role
 }
 
 output "scanner-function-policy" {
   description = "The policy attached to the scanner function role"
-  value = aws_iam_policy.antivirus-scanner-policy
+  value       = aws_iam_policy.antivirus-scanner-policy
 }
 
 output "update-function-policy" {
   description = "The policy attached to the definitions update function role"
-  value = aws_iam_policy.antivirus-update-policy
+  value       = aws_iam_policy.antivirus-update-policy
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,28 @@
 variable "buckets-to-scan" {
   description = "List of bucket names to be scanned by the antivirus"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "scanner-environment-variables" {
   description = "Custom environment variables for the scanner function"
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "updater-environment-variables" {
   description = "Custom environment variables for the definitions update function"
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "allow-public-access" {
   description = "If true, contents of the bucket in which the antivirus definitions are saved will be public. Good for sharing the same definitions across multiple accounts."
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "antivirus-update-rate" {
   description = "Configures the antivirus update rate. Syntax is the same of cloudwatch rate schedule expression for rules"
-  default = "3 hours"
+  type        = string
+  default     = "3 hours"
 }
