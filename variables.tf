05a84e0059433b546fc60963eb1b8d70e122b427
Gabriel Chamon Araujo <gchamon@live.com>
2019-09-28 22:28:05 -0300

    moved example to fit required strucutre and added descriptions

--- variables.tf
+++ variables.tf
@@ -1,16 +1,20 @@
 variable "buckets-to-scan" {
+  description = "List of bucket names to be scanned by the antivirus"
   type = list(string)
 }
 
 variable "scanner-environment-variables" {
+  description = "Custom environment variables for the scanner function"
   default = {}
 }
 
 variable "updater-environment-variables" {
+  description = "Custom environment variables for the definitions update function"
   default = {}
 }
 
 variable "allow-public-access" {
+  description = "If true, contents of the bucket in which the antivirus definitions are saved will be public. Good for sharing the same definitions across multiple accounts."
   default = false
 }
 
